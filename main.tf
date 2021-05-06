5084b9c0aa9fa77305e0eaed73cc3cea27b5aa64
Diego Gomez <diego.gomez@digitalonus.com>
2021-05-06 15:09:38 -0500

    Initial commit with gitignore

---
+++ main.tf
@@ -0,0 +1,11 @@
+data "azurerm_resource_group" "diegomez" {
+  name = "diego-gomez"
+}
+
+
+resource "azurerm_virtual_network" "test-network" {
+  name                = "dg-09-network"
+  address_space       = ["10.0.0.0/16"]p
+  location            = data.azurerm_resource_group.diegomez.location
+  resource_group_name = data.azurerm_resource_group.diegomez.name
+}
