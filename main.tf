bcb7e9fc3815ed5ed70967546058b8f8792f3073
Diego Gomez <diego.gomez@digitalonus.com>
2021-05-06 15:13:49 -0500

    typo error

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "azurerm_resource_group" "diegomez" {
 
 resource "azurerm_virtual_network" "test-network" {
   name                = "dg-09-network"
-  address_space       = ["10.0.0.0/16"]p
+  address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.diegomez.location
   resource_group_name = data.azurerm_resource_group.diegomez.name
 }
